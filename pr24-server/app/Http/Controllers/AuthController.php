55fb280ecc08b7749244a31a41b44dfcfff8e6a9
Ryan P. H. Chang <anakinchung930@gmail.com>
2024-03-25 11:12:03 +0800

    Update AuthController.php

--- pr24-server/app/Http/Controllers/AuthController.php
+++ pr24-server/app/Http/Controllers/AuthController.php
@@ -10,7 +10,7 @@ class AuthController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['login','register']]);
+        $this->middleware('auth:api', ['except' => ['login','register']]);
     }
 
     public function login(Request $request)
