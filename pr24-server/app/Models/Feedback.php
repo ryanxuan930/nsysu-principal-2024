c8232530b99e537693973dff4041f2d62c950e46
Ryan Chang <15924700+ryanxuan930@users.noreply.github.com>
2024-03-25 15:28:43 +0800

    Update Feedback.php

--- pr24-server/app/Models/Feedback.php
+++ pr24-server/app/Models/Feedback.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Feedback extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'feedback_id';
+
+    protected $fillable = [
+        'student_id',
+        'identity',
+        'college',
+        'grade',
+        'q1_score',
+        'q1_comment',
+        'q2_score',
+        'q2_comment',
+        'q3_comment',
+        'q4_score',
+        'q4_comment',
+        'q5_comment',
+        'q6_comment',
+    ];
+
+    protected $casts = [
+        'q1_score' => 'integer',
+        'q2_score' => 'integer',
+        'q4_score' => 'integer',
+    ];
 }
